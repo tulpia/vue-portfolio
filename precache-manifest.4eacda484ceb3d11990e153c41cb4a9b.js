9cdc7e481d2071304daa30abc77f9c73f2c2af1f
tulpia <teihotaata.alec@gmail.com>
2019-07-05 06:26:40 +0200

    deply

---
+++ precache-manifest.4eacda484ceb3d11990e153c41cb4a9b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f274b72eb1d09318d839",
+    "url": "/vue-portfolio/css/app.2b64c771.css"
+  },
+  {
+    "revision": "f274b72eb1d09318d839",
+    "url": "/vue-portfolio/js/app.64fc591b.js"
+  },
+  {
+    "revision": "aeec2224902bad2ad02c",
+    "url": "/vue-portfolio/js/chunk-vendors.ee5f585c.js"
+  },
+  {
+    "revision": "592b27341e997c4ee4e34deb49e07b83",
+    "url": "/vue-portfolio/img/victory.592b2734.png"
+  },
+  {
+    "revision": "c50cafe27126364ceb9f08dfd666645f",
+    "url": "/vue-portfolio/fonts/HelveticaNeueLTStd-Roman.c50cafe2.otf"
+  },
+  {
+    "revision": "7748644f3c21176ca49a2e37412d4535",
+    "url": "/vue-portfolio/fonts/FoundersGrotesk-Regular.7748644f.otf"
+  },
+  {
+    "revision": "2fbe417d6fe610f064b42acc360a5a58",
+    "url": "/vue-portfolio/fonts/FoundersGrotesk-Semibold.2fbe417d.otf"
+  },
+  {
+    "revision": "dc2f9f84f77fddceb36e607ced620ae7",
+    "url": "/vue-portfolio/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-portfolio/robots.txt"
+  },
+  {
+    "revision": "7d004e0ee2cf9303969844a924cde305",
+    "url": "/vue-portfolio/SplitText.js"
+  }
+];
